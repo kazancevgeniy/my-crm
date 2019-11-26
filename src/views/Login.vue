b2d5be26432033b36a7df543b885e2106c4d55ab
Evgeniy Kazantsev <ekazancev1922@gmail.com>
2019-11-26 16:32:35 +0300

    add validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,24 +1,26 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
             id="email"
             type="text"
-            class="validate"
+            v-model.trim="email"
+            :class="{invalid: isValidationEmailRequired || isValidationEmailIncorrect}"
         >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small class="helper-text invalid">{{getEmailValidateMessage}}</small>
       </div>
       <div class="input-field">
         <input
             id="password"
             type="password"
-            class="validate"
+            v-model.trim="password"
+            :class="{invalid: isValidationPasswordRequired || isValidationPasswordIncorrect}"
         >
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small class="helper-text invalid">{{getPasswordValidateMessage}}</small>
       </div>
     </div>
     <div class="card-action">
@@ -34,8 +36,81 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+import { email, required, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'Login',
+
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+  },
+
+  computed: {
+    isValidationEmailRequired() {
+      return this.$v.email.$dirty && !this.$v.email.required;
+    },
+
+    isValidationEmailIncorrect() {
+      return this.$v.email.$dirty && !this.$v.email.email;
+    },
+
+    getEmailValidateMessage() {
+      if (this.isValidationEmailRequired) {
+        return 'Поле обязательно для заполнения';
+      } if (this.isValidationEmailIncorrect) {
+        return 'Введите корректный email';
+      }
+      return '';
+    },
+
+    isValidationPasswordRequired() {
+      return this.$v.password.$dirty && !this.$v.password.required;
+    },
+
+    isValidationPasswordIncorrect() {
+      return this.$v.password.$dirty && !this.$v.password.minLength;
+    },
+
+    getValidationPasswordMinLength() {
+      return this.$v.password.$params.minLength.min;
+    },
+
+    getPasswordValidateMessage() {
+      if (this.isValidationPasswordRequired) {
+        return 'Введите пароль';
+      } if (this.isValidationPasswordIncorrect) {
+        return `Пароль должен содержать ${this.getValidationPasswordMinLength} символов. Сейчас он ${this.password.length}`;
+      }
+      return '';
+    },
+  },
+
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+      console.log(formData);
+      this.$router.push({ name: 'home' });
+    },
+  },
+};
+</script>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,36 +1,40 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
-            id="email"
-            type="text"
+          id="email"
+          type="text"
+          v-model.trim="email"
+          :class="{invalid: isValidationEmailRequired || isValidationEmailIncorrect}"
         >
         <label for="email">Email</label>
-        <small class="helper-text invalid">Email</small>
+        <small class="helper-text invalid">{{getEmailValidateMessage}}</small>
       </div>
       <div class="input-field">
         <input
-            id="password"
-            type="password"
-            class="validate"
+          id="password"
+          type="password"
+          v-model.trim="password"
+          :class="{invalid: isValidationPasswordRequired || isValidationPasswordIncorrect}"
         >
         <label for="password">Пароль</label>
-        <small class="helper-text invalid">Password</small>
+        <small class="helper-text invalid">{{getPasswordValidateMessage}}</small>
       </div>
       <div class="input-field">
         <input
             id="name"
             type="text"
-            class="validate"
+            v-model="name"
+            :class="{invalid: isValidationNameRequired}"
         >
         <label for="name">Имя</label>
-        <small class="helper-text invalid">Name</small>
+        <small v-if="isValidationNameRequired" class="helper-text invalid">Введите имя</small>
       </div>
       <p>
         <label>
-          <input type="checkbox" />
+          <input v-model="isAgree" type="checkbox" />
           <span>С правилами согласен</span>
         </label>
       </p>
@@ -48,8 +52,90 @@
 
       <p class="center">
         Уже есть аккаунт?
-        <a href="/">Войти!</a>
+        <router-link to="/">Войти!</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+import { email, required, minLength } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'Register',
+
+  data: () => ({
+    email: '',
+    password: '',
+    name: '',
+    isAgree: false,
+  }),
+
+  validations: {
+    email: { email, required },
+    password: { required, minLength: minLength(6) },
+    name: { required },
+    isAgree: { checked: v => v },
+  },
+
+  computed: {
+    isValidationEmailRequired() {
+      return this.$v.email.$dirty && !this.$v.email.required;
+    },
+
+    isValidationEmailIncorrect() {
+      return this.$v.email.$dirty && !this.$v.email.email;
+    },
+
+    getEmailValidateMessage() {
+      if (this.isValidationEmailRequired) {
+        return 'Поле обязательно для заполнения';
+      } if (this.isValidationEmailIncorrect) {
+        return 'Введите корректный email';
+      }
+      return '';
+    },
+
+    isValidationPasswordRequired() {
+      return this.$v.password.$dirty && !this.$v.password.required;
+    },
+
+    isValidationPasswordIncorrect() {
+      return this.$v.password.$dirty && !this.$v.password.minLength;
+    },
+
+    getValidationPasswordMinLength() {
+      return this.$v.password.$params.minLength.min;
+    },
+
+    getPasswordValidateMessage() {
+      if (this.isValidationPasswordRequired) {
+        return 'Введите пароль';
+      } if (this.isValidationPasswordIncorrect) {
+        return `Пароль должен содержать ${this.getValidationPasswordMinLength} символов. Сейчас он ${this.password.length}`;
+      }
+      return '';
+    },
+
+    isValidationNameRequired() {
+      return this.$v.name.$dirty && !this.$v.name.required;
+    },
+  },
+
+  methods: {
+    submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      const formData = {
+        email: this.email,
+        password: this.password,
+        name: this.name,
+      };
+      console.log(formData);
+      this.$router.push({ name: 'home' });
+    },
+  },
+};
+</script>
