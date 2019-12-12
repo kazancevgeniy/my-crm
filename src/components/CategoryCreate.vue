8d2357a9a54edd6225d8afc05f1f68c7a2ee5cf4
Evgeniy Kazantsev <ekazancev1922@gmail.com>
2019-12-12 14:24:21 +0300

    add create category

---
+++ src/components/CategoryCreate.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="col s12 m6">
+    <div>
+      <div class="page-subtitle">
+        <h4>Создать</h4>
+      </div>
+
+      <form @submit.prevent="submitHandler">
+        <div class="input-field">
+          <input
+            id="name"
+            type="text"
+            v-model="title"
+            :class="{ invalid: $v.title.$dirty && !$v.title.required }"
+          >
+          <label for="name">Название</label>
+          <span
+            v-if="$v.title.$dirty && !$v.title.required"
+            class="helper-text invalid"
+          >Введите название</span>
+        </div>
+
+        <div class="input-field">
+          <input
+            id="limit"
+            type="number"
+            v-model="limit"
+            :class="{ invalid: $v.limit.$dirty && !$v.limit.minValue }"
+          >
+          <label for="limit">Лимит</label>
+          <span
+            v-if="$v.limit.$dirty && !$v.limit.minValue"
+            class="helper-text invalid"
+          >Минимальная величина {{ $v.limit.$params.minValue.min }}</span>
+        </div>
+
+        <button class="btn waves-effect waves-light" type="submit">
+          Создать
+          <i class="material-icons right">send</i>
+        </button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { minValue, required } from 'vuelidate/lib/validators';
+
+export default {
+  name: 'CategoryCreate',
+  data: () => ({
+    title: '',
+    limit: 100,
+  }),
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) },
+  },
+  mounted() {
+    // eslint-disable-next-line no-undef
+    M.updateTextFields();
+  },
+  methods: {
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+      }
+      const category = await this.$store.dispatch('createCategory', {
+        title: this.title,
+        limit: this.limit,
+      });
+      this.limit = 100;
+      this.title = '';
+      this.$v.$reset();
+      this.$message('Категория была создана');
+      this.$emit('created', category);
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
